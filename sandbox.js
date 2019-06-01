db827fc3bf4919df2b6caeeabb8ee3893a1ea7a6
HopeNkiru <free2hopenkiru@gmail.com>
2019-06-01 18:42:28 +0100

    String Explanations

--- sandbox.js
+++ sandbox.js
@@ -8,13 +8,37 @@ const points = 100;
 
 console.log(points);
 
-Strings
+// Strings
+console.log('hello, world  11235');
+let email = 'mama@thematron.gmail.com';
+console.log(email);
 
 // String concatenation
+let firstName = 'Chime';
+let lastName = 'Hope';
+
+let fullName = firstName + ' '+ lastName;
+console.log(fullName);
 
 // getting characters
+console.log(fullName[0]);
 
 // String length
+console.log(fullName.length);
 
-// String methods
+// String methods - a string has different kinds of functions associated with it
+//these functions are called methods. A function is a snippet of code which 
+// performs some kind of specific task. A method is just a function which is associated with a particular object
+// or Data type.
+
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+
+
 
